96632d7e5b0eb6304a0f1956da774b81a89be6e9
KaterynaHolt <kholtvianskatech@gmail.com>
2023-05-17 03:39:22 +0300

    Added skeleton

---
+++ src/main/java/com/todolist/app/springmvcboottodolist/SpringMvcBootTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.app.springmvcboottodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBootTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcBootTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/app/springmvcboottodolist/SpringMvcBootTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.app.springmvcboottodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBootTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
