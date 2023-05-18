cccdf247d420a5697dd2dd9ad5c201a56e0f832a
KaterynaHolt <kholtvianskatech@gmail.com>
2023-05-18 03:37:41 +0300

    SMB-10-Create-simple-controller

--- src/main/java/com/todolist/app/springmvcboottodolist/SpringMvcBootTodoListApplication.java
+++ src/main/java/com/todolist/app/springmvcboottodolist/SpringMvcBootTodoListApplication.java
@@ -2,9 +2,11 @@ package com.todolist.app.springmvcboottodolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringMvcBootTodoListApplication {
+public class SpringMvcBootTodoListApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcBootTodoListApplication.class, args);
